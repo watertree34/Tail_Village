ad143cca7b90a1cd5a4b4cf45d32047412a3f0ad
ActNumber14 <68317209+ActNumber14@users.noreply.github.com>
2020-09-14 08:32:21 +0900

    게임클리어씬&인버티드스피어

---
+++ Tale_Village/Assets/JY/02.Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void OnClickExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+          Application.Quit();
+#endif
+    }
+}
