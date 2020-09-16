3e0c2556254df71be7765b70b7d796bf4c99e05e
ActNumber14 <68317209+ActNumber14@users.noreply.github.com>
2020-09-16 16:27:18 +0900

    VR연결

---
+++ Tale_Village/Assets/JY/02.Scripts/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject PcCam;
+    public GameObject VrObj;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
