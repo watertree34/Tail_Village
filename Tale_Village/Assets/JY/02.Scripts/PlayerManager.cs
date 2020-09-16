30d99284cde803ca380d4d9d08616297e0b214bf
ActNumber14 <68317209+ActNumber14@users.noreply.github.com>
2020-09-16 17:50:33 +0900

    VR/PC전환

--- Tale_Village/Assets/JY/02.Scripts/PlayerManager.cs
+++ Tale_Village/Assets/JY/02.Scripts/PlayerManager.cs
@@ -9,11 +9,25 @@ public class PlayerManager : MonoBehaviour
 
     void Start()
     {
-        
+#if UNITY_EDITOR || UNITY_EDITOR_WIN     //Unity 에디터 or Windows에선 PCcam 사용
+        PcCam.SetActive(true);
+        VrObj.SetActive(false);
+
+#else                                    //아니면 VR 사용
+        PcCam.SetActive(false);
+        VrObj.SetActive(true);
+#endif
     }
 
     void Update()
     {
-        
+#if UNITY_EDITOR || UNITY_EDITOR_WIN     //Unity 에디터 or Windows에선 PCcam 사용
+        PcCam.SetActive(true);
+        VrObj.SetActive(false);
+
+#else                                    //아니면 VR 사용
+        PcCam.SetActive(false);
+        VrObj.SetActive(true);
+#endif
     }
 }
