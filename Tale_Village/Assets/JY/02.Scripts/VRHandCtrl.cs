767851e57bd74ce95d582d17e810a7047bf4899b
watertree34 <40683453+watertree34@users.noreply.github.com>
2020-09-25 23:22:26 +0900

    레이 손 따로따로

--- Tale_Village/Assets/JY/02.Scripts/VRHandCtrl.cs
+++ Tale_Village/Assets/JY/02.Scripts/VRHandCtrl.cs
@@ -13,9 +13,9 @@ public class VRHandCtrl : MonoBehaviour
 
     void Start()
     {
-        layser = gameObject.GetComponent<LineRenderer>();
+        layser = GetComponent<LineRenderer>();
         // 레이저의 꼭지점 2개
-       // layser.positionCount = 2;
+        // layser.positionCount = 2;
         // 레이저 굵기 표현
         layser.startWidth = 0.02f;
         layser.endWidth = 0.02f;
@@ -26,68 +26,85 @@ public class VRHandCtrl : MonoBehaviour
 
         //layser.SetPosition(0, ARAVRInput.RHandPosition); // 첫번째 시작점 위치
         // 업데이트에 넣어 줌으로써, 플레이어가 이동하면 이동을 따라가게 된다.
-       
-        if (OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger))
+        if(controller==OVRInput.Controller.LTouch)
         {
-            layser.enabled = true;
-            layser.SetPosition(0,transform.position);
-            // 레이저에 감지된 것이 없기 때문에 레이저 초기 설정 길이만큼 길게 만든다.
-            layser.SetPosition(1, transform.position + (transform.forward * raycastDistance));
-        }
-       
-        else
-        {
-            
-             layser.enabled = false;
-        }
-
-
-            //선 만들기(충돌 감지를 위한)
-            Ray ray = new Ray(transform.position, transform.forward);
-            RaycastHit hitInfo;
-
-            // 충돌 감지 시
-            if (Physics.Raycast(ray, out hitInfo, 100))
+            if (OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger))
             {
-                layser.SetPosition(1, hitInfo.point);
-
-                // 충돌 객체의 태그가 Button인 경우
-                if (hitInfo.collider.gameObject.CompareTag("Button"))
-                {
-                    print("버튼 충돌");
-                    if (ARAVRInput.GetDown(ARAVRInput.Button.IndexTrigger, ARAVRInput.Controller.LTouch) || ARAVRInput.GetDown(ARAVRInput.Button.IndexTrigger, ARAVRInput.Controller.RTouch))
-                    {
-                        print("클릭");
-                        // 버튼에 등록된 onClick 메소드를 실행한다.
-                        hitInfo.transform.gameObject.GetComponent<Button>().onClick.Invoke();
-                        if (click == false)
-                        {
-                            click = true;
-                        }
-                    }
-                    else
-                    {
-                        hitInfo.transform.gameObject.GetComponent<Button>().OnPointerEnter(null);
-                        currentObject = hitInfo.collider.GetComponent<Collider>().gameObject;
-                        click = false;
-                    }
-                }
-
-
+                layser.enabled = true;
+                layser.SetPosition(0, transform.position);
+                // 레이저에 감지된 것이 없기 때문에 레이저 초기 설정 길이만큼 길게 만든다.
+                layser.SetPosition(1, transform.position + (transform.forward * raycastDistance));
             }
-            // 충돌 감지 X
             else
             {
 
-
-                // 최근 감지된 오브젝트가 Button인 경우
-                // 버튼은 현재 눌려있는 상태이므로 이것을 풀어준다.
-                if (currentObject != null)
-                {
-                    currentObject.GetComponent<Button>().OnPointerExit(null);
-                    currentObject = null;
-                }
-
+                layser.enabled = false;
             }
         }
+        else if(controller == OVRInput.Controller.RTouch)
+        {
+            if (OVRInput.Get(OVRInput.Touch.SecondaryIndexTrigger))
+            {
+                layser.enabled = true;
+                layser.SetPosition(0, transform.position);
+                // 레이저에 감지된 것이 없기 때문에 레이저 초기 설정 길이만큼 길게 만든다.
+                layser.SetPosition(1, transform.position + (transform.forward * raycastDistance));
+            }
+            else
+            {
+
+                layser.enabled = false;
+            }
+        }
+       
+
+
+        //선 만들기(충돌 감지를 위한)
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+
+        // 충돌 감지 시
+        if (Physics.Raycast(ray, out hitInfo, 100))
+        {
+            layser.SetPosition(1, hitInfo.point);
+
+            // 충돌 객체의 태그가 Button인 경우
+            if (hitInfo.collider.gameObject.CompareTag("Button"))
+            {
+                print("버튼 충돌");
+                if (ARAVRInput.GetDown(ARAVRInput.Button.IndexTrigger, ARAVRInput.Controller.LTouch) || ARAVRInput.GetDown(ARAVRInput.Button.IndexTrigger, ARAVRInput.Controller.RTouch))
+                {
+                    print("클릭");
+                    // 버튼에 등록된 onClick 메소드를 실행한다.
+                    hitInfo.transform.gameObject.GetComponent<Button>().onClick.Invoke();
+                    if (click == false)
+                    {
+                        click = true;
+                    }
+                }
+                else
+                {
+                    hitInfo.transform.gameObject.GetComponent<Button>().OnPointerEnter(null);
+                    currentObject = hitInfo.collider.GetComponent<Collider>().gameObject;
+                    click = false;
+                }
+            }
+
+
+        }
+        // 충돌 감지 X
+        else
+        {
+
+
+            // 최근 감지된 오브젝트가 Button인 경우
+            // 버튼은 현재 눌려있는 상태이므로 이것을 풀어준다.
+            if (currentObject != null)
+            {
+                currentObject.GetComponent<Button>().OnPointerExit(null);
+                currentObject = null;
+            }
+
+        }
     }
+}
